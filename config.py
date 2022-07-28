bb991928ce0fe901df081e59bb0d144600532137
VitBorkin <110170330+VitBorkin@users.noreply.github.com>
2022-07-28 20:45:45 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Consist Tokens for Telegram Bot and for payment authorization
-
-TOKEN = '5578475141:AAHkZVaMnyReebfxrSUTpdVr78SCOq1Py8w'
-
-# TODO Добавить токен для платёжной системы, сделать ссылку на приложение оплаты - payments
